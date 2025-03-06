c797505fed64f16c062de4acf367145f80a054ff
dziewulskij <jaroslaw.dziewulski.kontakt@gmail.com>
2025-03-06 22:58:03 +0100

    Init commit

---
+++ src/main/java/pl/dziewulskij/tradepoint/TradePointApplication.java
@@ -0,0 +1,13 @@
+package pl.dziewulskij.tradepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradePointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradePointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dziewulskij/tradepoint/TradePointApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dziewulskij.tradepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradePointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
